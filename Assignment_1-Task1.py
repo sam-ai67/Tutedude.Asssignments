88c9b82fa3b4183d544634ba8d6de279ab9e1515
sam-ai67 <personalsam67@gmail.com>
2025-06-14 17:06:19 +0530

    Add files via upload

---
+++ Assignment_1-Task1.py
@@ -0,0 +1,8 @@
+#Task 1: Perform Basic Mathematical Operations
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+print("\n")
+print("Addition: ",x+y)
+print("Subtraction: ",x-y)
+print("Multiplication: ",x*y)
+print("Division: ",x/y)
---
+++ Assignment_1-Task2.py
@@ -0,0 +1,4 @@
+x=input("Enter your first name: ")
+y=input("Enter your last name: ")
+print("\n")
+print("Hello,{} {}!Welcome to the program.".format(x,y))
